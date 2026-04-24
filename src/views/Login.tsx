2e3440ba242d942dfb7fd5d9f2fab8f56700dada
Amit Kavathekar <amitkavathekar123@gmail.com>
2026-04-24 10:12:55 +0530

    update title name in inde.html

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -8,7 +8,7 @@ import {
   CardDescription,
   CardFooter,
   CardHeader,
-  CardTitle,
+  CardTitle,  
 } from "../components/ui/card"
 import { Input } from "../components/ui/input"
 import { Label } from "../components/ui/label"
