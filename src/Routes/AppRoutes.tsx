4ee0509ab6f1b4c59f5ee8cbc12976c51de27373
Amit Kavathekar <amitkavathekar123@gmail.com>
2026-04-26 15:22:04 +0530

    remove home.jsx page

--- src/Routes/AppRoutes.tsx
+++ src/Routes/AppRoutes.tsx
@@ -3,12 +3,10 @@ import Login from "../views//Login"
 import Register from "../views//Register"
 import ForgotPassword from "../views/ForgotPassword"
 import Dashboard from "../views//Dashboard"
-import Home from "../views/Home"
 
 function AppRoutes() {
   return (
      <Routes>
-      <Route path="/" element={<Home />} />
       <Route path="/register" element={<Register />} />
       <Route path="/login" element={<Login />} />
       <Route path="/forgot" element={<ForgotPassword />} />
